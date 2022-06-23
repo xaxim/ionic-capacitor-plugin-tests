542ff82c5f44a4f11f1ea6c1e0e4e91b10aeb26b
Carlos Lima <carloslima@mpdft.mp.br>
2022-06-23 13:42:07 -0300

    Added ios and android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.bsb.carlos',
   appName: 'ionic-capacitor-plugin-tests',
   webDir: 'www',
   bundledWebRuntime: false
